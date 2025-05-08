29ace2e9678b2a6fdb3f6f01fae8ca7ec5943df0
Maf1sher <kubas307@gmail.com>
2025-05-08 22:25:07 +0200

    User and Role Entity

---
+++ MualaFuel-Backend/src/main/java/com/example/MualaFuel_Backend/entity/Role.java
@@ -0,0 +1,22 @@
+package com.example.MualaFuel_Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "role")
+@Table(name = "role")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+}
